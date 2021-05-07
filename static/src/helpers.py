1e0bd0724d6274edbc0f0500031906850e042b16
Damianus Deni <damianus.deni@gmail.com>
2021-05-07 10:07:42 +0700

    omit double functions of get_tweet

--- static/src/helpers.py
+++ static/src/helpers.py
@@ -174,27 +174,6 @@ def get_tweets(keyword='indonesia', location="-0.789275,113.921326,5000km", lang
         time.sleep(3)
 
 
-# def get_tweets(text_query):
-    # list to store tweets
-    tweets_list = []
-    # no of tweets
-    count = 50
-    try:
-        # Pulling individual tweets from query
-        for tweet in api.search(q=text_query, count=count):
-            # print(tweet.text)
-            # Adding to list that contains all tweets
-            tweets_list.append({'created_at': tweet.created_at,
-                                'tweet_id': tweet.id,
-                                'tweet_text': tweet.text})
-        # return tweets_list
-        return pd.DataFrame.from_dict(tweets_list)
-
-    except BaseException as e:
-        print('failed on_status,', str(e))
-        time.sleep(3)
-
-
 ######## PREDICT TWITTER TEXT ########
 def predict_sentiment(model, text_list, colname='tweet_text_preprocessed'):
     y_pred_desc_list = []
